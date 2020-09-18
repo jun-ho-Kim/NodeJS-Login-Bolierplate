09c2836cbe9d7c0fedd39066d30f4e5ceba70737
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-09-18 15:01:21 +0900

    Post UserInfo to DB

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 const bodyPaser = require('body-parser');
+
 const config = require('./config/key');
+
+const {User} = require("./models/User");
 
 app.use(bodyPaser.urlencoded({extended: true}));
 app.use(bodyPaser.json());
@@ -14,4 +17,16 @@ mongoose.connect(config.mongoURI, {
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World 안녕하세요~~~'));
+app.post('/register', (req,res) => {
+    //회원가입할 때 필요한 정보들을 client에 가져오면
+    //그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, userInfro) => {
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
+
 app.listen(port, () => console.log(`✅  Example app listening on port ${port}!`))
