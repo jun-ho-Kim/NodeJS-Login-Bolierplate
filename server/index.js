928293aacd6c853a48fbecdff7f248f07bb064d3
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-09-18 11:28:07 +0900

    Connect MongoDB, Create UserSchema

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const bodyPaser = require('body-parser');
+const config = require('./config/key');
+
+app.use(bodyPaser.urlencoded({extended: true}));
+app.use(bodyPaser.json());
+
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('✅  MongoDB Connected...'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World 안녕하세요~~~'));
 app.listen(port, () => console.log(`✅  Example app listening on port ${port}!`))
