8ab52f8b83f3a6ec2d4cd279b9f4215a1a08d60d
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-09-21 11:54:36 +0900

    bcrypt

--- server/index.js
+++ server/index.js
@@ -20,13 +20,33 @@ app.get('/', (req, res) => res.send('Hello World 안녕하세요~~~'));
 app.post('/register', (req,res) => {
     //회원가입할 때 필요한 정보들을 client에 가져오면
     //그것들을 데이터베이스에 넣어준다.
-    const user = new User(req.body)
-    user.save((err, userInfro) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
         if(err) return res.json({success: false, err});
         return res.status(200).json({
             success: true
         });
     });
 });
+
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
+        user.comparePassword(req.body.password, (err,isMatch) => {
+            if(err) return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+        //비밀번호 까지 맞다면 토큰 생성하기
+        user.generateToken((err, user) => {
+        
+        })
+        
+        })
+    })
+})
 
 app.listen(port, () => console.log(`✅  Example app listening on port ${port}!`))
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; //자리수
 
 const userSchema = mongoose.Schema({
     name: {
@@ -32,5 +34,33 @@ const userSchema = mongoose.Schema({
 })
 
 const User = mongoose.model("User", userSchema);
+
+//userSchema.pre, userSchema.method.newFucntion은 mongoose method
+userSchema.pre('save', function(next){
+    var user = this;
+    //비밀번호가 바뀔 경우 user.password를 bcrypt를 통해 암호화한다.
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err,salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+});
+
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+};
+
+
 
 module.exports = {User};
