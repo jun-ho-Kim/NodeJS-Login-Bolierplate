c5f19b9b2fdc21e6b2f094b05194979eba15c138
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-09-23 15:31:09 +0900

    react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage.js";
 import LoginPage from "./components/views/LoginPage/LoginPage.js";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage.js";
@@ -8,13 +8,13 @@ import RegisterPage from "./components/views/RegisterPage/RegisterPage.js";
 function App() {
   return (
     <Router>
-      {/* <div>
+      <div>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/register" component={RegisterPage} />
         </Switch>
-      </div> */}
+      </div>
     </Router>
   );
 }
