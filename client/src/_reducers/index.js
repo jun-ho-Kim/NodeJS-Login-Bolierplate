d26b4f6d4e0f7d9ffdafaf33c0c0adec7c26f0f6
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-09-24 12:17:56 +0900

    Redux Setting

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+
+const rootReducer = combineReducers({
+    // reducer인데 나눠줘 있는 reducer들을 
+    // 하나로 합쳐준다.(Combine Reducer)
+});
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import 'antd/dist/antd.css';
+import {createStore, applyMiddleware, combineReducers} from "redux";
+import ReduxThunk from "redux-thunk";
+import promiseMiddleware from "redux-promise";
+import Reducer from "./_reducers"
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk, promiseMiddleware)(createStore);
 
 ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(Reducer,
+    //Redux DevTools를 사용하기 위해 세팅
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  
+    )}
+  >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
